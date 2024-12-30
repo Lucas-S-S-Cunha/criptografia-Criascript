011dae509b0997502fc1d51f92b387b1b09c249e
Lucas-S-S-Cunha <lucassasocunha@gmail.com>
2024-12-30 00:40:53 -0300

    adição das funções

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const letras = "abcdefghijklmnopqrstuvwxyz"
 const palavra = "xVlIoo"//dijite aqui a palavra da qual deseja criptografar
 
 //Exemplo utilizando indexOf
+function cripWithIndexOf(palavra) {
+    
 const indices = Array.from(palavra.toLocaleLowerCase()).map(letra => letras.indexOf(letra) + 1)
 console.log(indices)
 
+}
 //exemplo utilizando chartAt/chartCodeAt
+function cripWithCharCodeAt(palavra) {
+
 const posicoes = Array.from(palavra.toLocaleLowerCase()).map(letra => letra.charCodeAt(0) - 'a'.charCodeAt(0) + 1)
 console.log(posicoes)
+
+}
+cripWithIndexOf(palavra)
+cripWithCharCodeAt(palavra)
